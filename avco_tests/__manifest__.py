dc0afa7b6401e448ca1258f84095292bf0c0fd00
Humberto Arocha <hbto@vauxoo.com>
2017-07-30 17:06:29 -0400

    [ADD] Added scaffolding module for testing AVCO Costing Method

---
+++ avco_tests/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Odoo Average Costing Method Unit Tests',
+    'version' : '1.0',
+    'summary': 'Unit Tests for Average Costing Method',
+    'sequence': 30,
+    'description': """
+    AVCO Unit Tests
+    """,
+    'category': 'Accounting',
+    'website': 'https://www.vauxoo.com/',
+    'images' : [],
+    'depends' : ['stock_account', 'sale_stock', 'stock_landed_costs'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
